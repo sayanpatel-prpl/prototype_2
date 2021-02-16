1e29ea416c42ee6e9cf4b117fdd6bfdb90b98bc1
Sayan Patel <sayanpatel98@gmail.com>
2021-02-16 13:12:34 +0530

    Setting the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :videos
+  resources :videos, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'videos#new'
 end
