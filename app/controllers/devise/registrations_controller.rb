94fc50446f0fce5aecd8e2116d57f081fb424a44
Sayan Patel <sayanpatel98@gmail.com>
2021-02-21 16:14:43 +0530

    Setting up mailer views

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -15,7 +15,6 @@ class Devise::RegistrationsController < DeviseController
   # POST /resource
   def create
     build_resource(sign_up_params)
-
     resource.save
     yield resource if block_given?
     if resource.persisted?
